2d52a4c53d02f0d57196cc3bd954d49b7f97bf4d
Vinh_tog <tongthinhvinh12@gmail.com>
2023-12-10 15:53:01 +0700

    add if else and operation

--- print.py
+++ print.py
@@ -5,3 +5,6 @@ print(a,b,c)
 print('python', 'java', 'c++')
 print('python', 'java', 'c++', sep='#########')
 print('python', 'java', 'c++', end='%%')
+print("\n====================");
+
+print(0x2A)
